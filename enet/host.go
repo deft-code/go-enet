a68db6909c03585d35f6de7a4e30a58a10da5208
Nick Terry <j.nick.terry@gmail.com>
2012-06-12 08:05:11 -0700

    Migrate away from ENetPacket (use []byte instead) Wrap ENetPointers in go types

--- enet/host.go
+++ enet/host.go
@@ -10,32 +10,37 @@ import "log"
 import "net"
 import "unsafe"
 
-type ENetHost *C.ENetHost
+type Host struct {
+   host *C.ENetHost
+}
 
 func conv_addr(address net.UDPAddr) (ret C.ENetAddress) {
 	c_ip := C.CString(address.IP.String())
+   defer C.free(c_ip)
+
 	C.enet_address_set_host(&ret, c_ip)
 	ret.port = C.enet_uint16(address.Port)
 	return
 }
 
 // enet_host_create
-func CreateHost(address net.UDPAddr, peerCount uint, channelLimit uint, incomingBandwidth uint32, outgoingBandwith uint32) ENetHost {
+func CreateHost(address net.UDPAddr, peerCount uint, channelLimit uint, incomingBandwidth uint32, outgoingBandwith uint32) Host {
 	addr := conv_addr(address)
 	host := C.enet_host_create(&addr, C.size_t(peerCount), C.size_t(channelLimit), C.enet_uint32(incomingBandwidth), C.enet_uint32(outgoingBandwith))
 	if unsafe.Pointer(host) == unsafe.Pointer(0) {
 		log.Fatal("TODO return an error here")
 	}
-	return host
+	return Host{host}
 }
 
 // enet_host_destory
-func (host ENetHost) Destory() {
-	C.enet_host_destroy(host)
+func (host Host) Destory() {
+	C.enet_host_destroy(host.host)
+   host.host = null
 }
 
 // enet_host_connet
-func (host ENetHost) Connect(address net.UDPAddr, channelCount uint, data uint) ENetPeer {
+func (host Host) Connect(address net.UDPAddr, channelCount uint, data uint) ENetPeer {
 	addr := conv_addr(address)
 	peer := C.enet_host_connect(host, &addr, C.size_t(channelCount), C.enet_uint32(data))
 	if unsafe.Pointer(peer) == unsafe.Pointer(0) {
@@ -45,18 +50,18 @@ func (host ENetHost) Connect(address net.UDPAddr, channelCount uint, data uint) 
 }
 
 // enet_host_flush
-func (host ENetHost) Flush() {
+func (host Host) Flush() {
 	C.enet_host_flush()
 }
 
 type EnetEvent *C.ENetEvent
 
 // enet_host_service
-func (host ENetHost) Service(event ENetEvent, timeout_ms uint) int {
+func (host Host) Service(event ENetEvent, timeout_ms uint) int {
 	return int(C.enet_host_service(host, event, C.enet_uint32(timeout_ms)))
 }
 
 // enet_host_broadcast
-func (host ENetHost) Broadcast(channelID uint8, packet ENetPacket) {
+func (host Host) Broadcast(channelID uint8, packet ENetPacket) {
 	C.enet_host_broadcast(C.enet_uint8(channelID), packet)
 }
--- enet/peer.go
+++ enet/peer.go
@@ -10,17 +10,43 @@ type Peer struct {
    peer *C.ENetPeer
 }
 
+func new_packet( data []bytes, flags uint ) *C.ENetPacket {
+	packet := C.enet_packet_create(unsafe.Pointer(&data[0]), C.size_t(len(data)), C.enet_uint32(flags))
+	if unsafe.Pointer(packet) == unsafe.Pointer(uintptr(0)) {
+      panic("this should never happen")
+	}
+	return packet
+}
+
+func from_packet( packet *C.ENetPacket ) []byte {
+   ret := make([]byte, packet.dataLength)
+   for i:=0; i<len(ret); i++ {
+      ret[i] = byte(packet.data[i])
+   }
+}
+
+func zero_or_error( val C.int) error {
+   if val == 0 {
+      return error{ret}
+   } else {
+      return nil
+   }
+}
+
 // enet_peer_send
-func (peer Peer) Send(channelID uint8, packet Packet) int {
-	return int(C.enet_peer_send(peer, C.enet_uint8(channelID), packet))
+func (peer Peer) Send(channelID uint8, data []byte, flags uint) error {
+   packet := new_packet( data, flags )
+   defer C.enet_packet_destroy(packet)
+
+   return zero_or_error( C.enet_peer_send(
+         peer.peer, C.enet_uint8(channelID), packet))
 }
 
 // enet_peer_receive
-func (peer Peer) Receive() (channelID uint8, packet []byte) {
-	var c_channel_id C.enet_uint8
-	packet = C.enet_peer_receive(peer, &c_channel_id, packet)
-	channelID = c_channel_id
-	return
+func (peer Peer) Receive() ([]byte,uint8)  {
+	var channel_id C.enet_uint8 = 0
+	packet := C.enet_peer_receive(peer.peer, &channel_id)
+	return from_packet(packet), uint8(channel_id)
 }
 
 // enet_peer_reset
